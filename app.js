2b995f5ce607ab3f264deda2abdd8fbe188c4031
Beatriz-moises <beatrizmoises5@gmail.com>
2020-02-12 13:13:11 -0500

    alumno-Ofline

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+document.addEventListener("DOMContentLoaded",e=>{document.querySelector("#frmAlumnos").addEventListener("submit",event=>{event.preventDefault();
+    let codigo= document.querySelector("#txtcodigoAlumno").value,
+        nombre= document.querySelector
+
+document.addEventListener("DOMContentLoaded",function(e){alert("CALLBACK LISTO");});
