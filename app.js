ade3a76dbb72c0692a26a3702106b15a00179637
Beatriz-moises <60196298+Beatriz-moises@users.noreply.github.com>
2020-02-13 14:12:03 -0500

    Alumno-Offline

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ function limpiar(){
         document.querySelector("#txtcodigoAlumno").value = "";
         document.querySelector("#txtnombreAlumno").value = "";
         document.querySelector("#txtapellidoAlumno").value = "";
-        document.querySelector("#txtfechaAlumno").value = "";
         document.querySelector("#txttelAlumno").value="";
         document.querySelector("#txtdireccionAlumno").value="";
 }
@@ -27,17 +26,15 @@ function guardarRegistro(e){
     let codigo=document.querySelector("#txtcodigoAlumno").value,
     nombre=document.querySelector("#txtnombreAlumno").value,
     apellido=document.querySelector("#txtapellidoAlumno").value,
-    fecha=document.querySelector("#txtfechaAlumno").value,
     telefono=document.querySelector("#txttelAlumno").value,
     direccion=document.querySelector("#txtdireccionAlumno").value;
 
-    console.log(nombre, apellido, fecha, telefono, direccion);
+    console.log(nombre, apellido, telefono, direccion);
 
     if( 'localStorage' in window ){
         window.localStorage.setItem("codigo" + codigo, codigo);
         window.localStorage.setItem("nombre" + codigo, nombre);
         window.localStorage.setItem("apellido" + codigo, apellido);
-        window.localStorage.setItem("fech" + codigo, fecha);
         window.localStorage.setItem("direccion" + codigo, direccion);
         window.localStorage.setItem("telefono" + codigo, telefono);
     } else {
@@ -49,7 +46,6 @@ function buscar(){
     let codigo=document.querySelector("#txtcodigoAlumno").value;
     document.querySelector("#txtnombreAlumno").value=window.localStorage.getItem("nombre" + codigo);
     document.querySelector("#txtapellidoAlumno").value=window.localStorage.getItem("apellido" + codigo);
-    document.querySelector("#txtfechaAlumno").value=window.localStorage.getItem("fecha" + codigo);
     document.querySelector("#txttelAlumno").value=window.localStorage.getItem("telefono" + codigo);
     document.querySelector("#txtdireccionAlumno").value=window.localStorage.getItem("direccion" + codigo);
 
