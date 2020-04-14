38b684a5fdef97efe2094e18e84c5fe5185b449b
Beatriz-moises <75246265+Beatriz-moises@users.noreply.github.com>
2020-04-14 01:18:21 -0600

    app

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,10 @@ function init(){
     $("[class*='mostrar']").click(function(e){
         let modulo = $(this).data("modulo"),
             form   = $(this).data("form");
-        
-        $(`#vista-${form}`).load(`public/vistas/${modulo}/${form}.html`, function(){
+
+        $(`#vistas-${form}`).load(`public/vistas/${modulo}/${form}.html`, function(){
             $(`#btn-close-${form}`).click(()=>{
-                $(`#vista-${form}`).html("");
+                $(`#vistas-${form}`).html("");
             });
             init();
         }).draggable();
